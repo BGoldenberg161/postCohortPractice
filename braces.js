66e2d5414af4724d963d4d010160313ac2b7fda3
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-10-22 12:27:20 -0600

    solved braces problem

--- braces.js
+++ braces.js
@@ -1,35 +1,20 @@
 function validBraces(braces){
-    const bracesArr = braces.split('')
-    prevBraces = []
-    bracesArr.forEach(b => {
-      if (b === '[' || b === '(' || b === '{'){
-        prevBraces.push(b)
-        console.log(prevBraces)
-      } else if (b === ']') {
-          if (prevBraces[prevBraces.length - 1] === '['){
-              prevBraces.pop()
-              console.log(prevBraces)
-          } else {
-              return false
-          }
-      } else if (b === ')') {
-        if (prevBraces[prevBraces.length - 1] === '('){
-            prevBraces.pop()
-            console.log(prevBraces)
-        } else {
-            return false
-        }
-      } else if ( b === '}') {
-        if (prevBraces[prevBraces.length - 1] === '{'){
-            prevBraces.pop()
-            console.log(prevBraces)
-        } else {
-            return false
-        }
-      }
-    })
-    if (prevBraces.length > 0) return false
-    else return true
+  const bracesArr = braces.split('')
+  let prevBraces = []
+  
+  for (let i=0; i <= bracesArr.length; i++) {
+    if (bracesArr[i] === '[' || bracesArr[i] === '(' || bracesArr[i] === '{'){
+      prevBraces.push(bracesArr[i])
+    } else if ( bracesArr[i] === ']' && prevBraces[prevBraces.length -1] === '[') {
+        prevBraces.pop()
+    } else if ( bracesArr[i] === ')' && prevBraces[prevBraces.length - 1] === '(') {
+        prevBraces.pop()
+    } else if ( bracesArr[i] === '}' && prevBraces[prevBraces.length - 1] === '{') {
+        prevBraces.pop()
+    } else if (bracesArr[i] === '}' || bracesArr[i] === ')' || bracesArr[i] === ']'){
+        return false
+    }
+  }
+  if (prevBraces.length > 0) return false
+  else return true
 }
-
-  console.log(validBraces( "}}]]))}])" ))
